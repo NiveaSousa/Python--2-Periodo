a9d1243d6cedc4f40972023aa0e9a9c6f42a9dd2
Nivea Ester <88627342+NiveaSousa@users.noreply.github.com>
2022-09-23 12:45:35 -0300

    Delete
    Organização em pastas

--- Dic - Cheat 1.py
+++
@@ -1,8 +0,0 @@
-dados = {}
-
-dados['nome'] = input('Digite nome: ')
-dados['idade'] = input('Digite idade: ')
-dados['telefone'] = input('Digite telefone: ')
-dados['endereço'] = input('Digite endereço: ')
-
-print(dados)
